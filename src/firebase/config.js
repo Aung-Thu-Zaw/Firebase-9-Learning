1f6feab1c81d2a9a25c9c38078cda4b6fdbe4c03
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2022-10-21 10:32:20 +0630

    initialize firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASe-sSM8_kUAYtDtv2z7fETk6Y_10AM48",
+  authDomain: "fir-9-learning-8d148.firebaseapp.com",
+  projectId: "fir-9-learning-8d148",
+  storageBucket: "fir-9-learning-8d148.appspot.com",
+  messagingSenderId: "368570412204",
+  appId: "1:368570412204:web:1f867722bbc7cb35d24309",
+};
+
+initializeApp(firebaseConfig);
